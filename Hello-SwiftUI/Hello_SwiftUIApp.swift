4f5fcc26175e8eab81a80189540f7a17254e323f
luciarigoni <luciarigoni@live.com>
2022-11-10 13:38:40 -0300

    Initial Commit

---
+++ Hello-SwiftUI/Hello_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  Hello_SwiftUIApp.swift
+//  Hello-SwiftUI
+//
+//  Created by Lucia Rigoni Freire on 10/11/22.
+//
+
+import SwiftUI
+
+@main
+struct Hello_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
